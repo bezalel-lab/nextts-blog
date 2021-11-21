ca89dc8b06be90be5ab01af1f50755d60181981c
tomo <63831701+techwork25@users.noreply.github.com>
2021-11-21 19:43:10 +0900

    Update script.js

--- public/calculateAmounts/js/script.js
+++ public/calculateAmounts/js/script.js
@@ -29,7 +29,8 @@ jQuery("input").on("change", function() {
     localStorage.setItem("date" + i, input[i].value);
     localStorage.setItem("amount" + i, input[i + 4].value);
     localStorage.setItem("remark" + i, input[i + 8].value);
-  }  
+  }
+  displayResults();
 })
 
 
@@ -40,12 +41,25 @@ function displayResults() {
   jQuery(".calculate__total").text(total);
 
   jQuery(".calculate__text-month").text(localStorage.getItem("yearMonth"));
-  
+
   for (let i = 1; i < 5; i++) {
-    jQuery(".calculate__text-date" + i).text(localStorage.getItem("date" + i));
-    jQuery(".calculate__text-amount" + i).text(localStorage.getItem("amount" + i));
+      jQuery(".calculate__text-date" + i).text(localStorage.getItem("date" + i));
+      jQuery(".calculate__text-amount" + i).text(localStorage.getItem("amount" + i));
+      
     if (localStorage.getItem("remark")) {
-      jQuery(".calculate__text-remark" + i).text("（" +localStorage.getItem("remark" + i) + "）");
+      jQuery(".calculate__text-remark" + i).text("（" +localStoage.gretItem("remark" + i) + "）");
+    }
+  }
+
+  toggleShowAndHide()
+}
+
+function toggleShowAndHide() {
+  for (let i = 1; i < 5; i++) {
+    if (localStorage.getItem("date" + i)) {
+      jQuery(".text__content" + i).addClass("is-show");
+    } else {
+      jQuery(".text__content" + i).removeClass("is-show");
     }
   }
 }
