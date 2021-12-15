cdd9f30aca8d76506821038e4bd4b42ca5434f74
tomo <63831701+shieteck@users.noreply.github.com>
2021-12-15 15:26:43 +0900

    Partially commented in

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,12 @@ import Line from '../components/Line';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
-// import Image from 'next/link';
+//import Image from 'next/link';
 import Learn from '../components/learn';
 import Date from '../components/date';
 import { GetStaticProps } from 'next';
 import firebase from '../firebase/clientApp';
-// Import the useAuthStateHook
+//Import the useAuthStateHook
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import Auth from '../components/Auth';
